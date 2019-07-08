354793743e7851138c4951599b473b4ace519057
Ankit Kumar <ankit230698@gmail.com>
2019-07-08 14:41:40 +0530

    ghshjd

--- backend/models/post.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const postSchema = mongoose.Schema({
-  title: {type: String, required: true}, //default: 'Hello'
-  content: {type: String}
-});
-
-module.exports = mongoose.model('Post', postSchema);
